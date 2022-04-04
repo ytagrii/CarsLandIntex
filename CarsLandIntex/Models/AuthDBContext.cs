6a66e98a80d5945d9d3b9d9442dabe7ca6365fe6
ytagrii <72105289+ytagrii@users.noreply.github.com>
2022-04-04 10:28:29 -0600

    new

---
+++ CarsLandIntex/Models/AuthDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarsLandIntex.Models
+{
+    public class AuthDBContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthDBContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
--- CarsLandIntex/Startup.cs
+++ CarsLandIntex/Startup.cs
@@ -12,6 +12,7 @@ using CarsLandIntex.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using CarsLandIntex.Models;
 
 namespace CarsLandIntex
 {
@@ -27,11 +28,18 @@ namespace CarsLandIntex
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            //services.AddDbContext<ApplicationDbContext>(options =>
+            //    options.UseSqlite(
+            //        Configuration.GetConnectionString("DefaultConnection")));
+            services.AddDbContext<AuthDBContext>(options =>
+            {
+                options.UseMySql(Configuration["ConnectionStrings:AuthConnection"]);
+            });
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<AuthDBContext>();
+
+            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //    .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
         }
