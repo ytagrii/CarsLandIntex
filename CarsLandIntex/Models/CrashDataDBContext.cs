88d1899f4ab464390092ccda6ba4a0bf3534dec0
ytagrii <72105289+ytagrii@users.noreply.github.com>
2022-04-04 17:30:48 -0600

    repo stuff

---
+++ CarsLandIntex/Models/CrashDataDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarsLandIntex.Models
+{
+    public class CrashDataDBContext : DbContext
+    {
+        public CrashDataDBContext() { }
+        public CrashDataDBContext(DbContextOptions<CrashDataDBContext> options) : base(options)
+        {
+
+        }
+
+        //this is what migrations will reconize
+        public DbSet<Crash> Crashes { get; set; }
+        public DbSet<Severity> Severities { get; set; }
+    }
+}
