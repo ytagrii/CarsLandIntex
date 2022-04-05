864af55b6f1dfa28c0b7739f63f29305ab829f43
levimorse <63205032+levimorse@users.noreply.github.com>
2022-04-05 14:55:49 -0600

    County Model

--- CarsLandIntex/Models/County.cs
+++ CarsLandIntex/Models/County.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace CarsLandIntex.Models
 {
     public class County
     {
-        public County()
-        {
-        }
+        [Key]
+        [Required]
+        public int COUNTY_ID { get; set; }
+        [Required]
+        public string COUNTY_NAME { get; set; }
     }
 }
