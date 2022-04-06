23e532c58e1e020ad8ad0df8455a561f5231f774
levimorse <63205032+levimorse@users.noreply.github.com>
2022-04-06 12:11:00 -0600

    chang

--- CarsLandIntex/wwwroot/js/main.js
+++ CarsLandIntex/wwwroot/js/main.js
@@ -6,7 +6,7 @@
 
 $(document).ready(
 	function () {
-		$('#nav-icon1,#nav-icon2,#nav-icon3,#nav-icon4').click(function () {
+		$('#nav-icon3').click(function () {
 			$(this).toggleClass('open');
 		});
 });
