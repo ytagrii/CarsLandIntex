acdae66a2d7114b9caceff30ce4f5f26f1028edd
levimorse <63205032+levimorse@users.noreply.github.com>
2022-04-07 15:45:45 -0600

    poiu

--- CarsLandIntex/wwwroot/js/main.js
+++ CarsLandIntex/wwwroot/js/main.js
@@ -4,6 +4,31 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+// define timeline
+const tl = gsap.timeline({ defaults: { duration: 0.4, ease: "power1.out" } });
+
+// slide up cookie bar
+tl.fromTo(".cookie-wrapper", { y: 400 }, { y: 0 });
+
+//cookie jump animation
+tl.fromTo(
+	".cookie",
+	{ y: 0, rotation: 0 },
+	{ y: -20, rotation: "-20deg", yoyo: true, repeat: -1 }
+);
+
+// animate cookie crumbs
+tl.fromTo("#crumbs", { y: 0 }, { y: -20, yoyo: true, repeat: -1 }, "<");
+
+// get button
+const cookieBtn = document.querySelector(".cookie-btn");
+
+// slide down cookie bar on btn click
+cookieBtn.addEventListener("click", () => {
+	gsap.to(".cookie-wrapper", { y: 400, duration: 0.4, ease: "power1.out" });
+});
+
+
 $(document).ready(
 	function () {
 		$('#nav-icon3').click(function () {
