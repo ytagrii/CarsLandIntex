f058d162fd8d35d62c4278c405a094ebd77902b9
ytagrii <72105289+ytagrii@users.noreply.github.com>
2022-04-07 11:43:14 -0600

    Update Filtering.cs

--- CarsLandIntex/Models/Filtering.cs
+++ CarsLandIntex/Models/Filtering.cs
@@ -3,6 +3,7 @@ namespace CarsLandIntex.Models
 {
     public class Filtering
     {
+        
         public int? county { get; set; }
         public string? city { get; set; }
         public string? weekday { get; set; }
